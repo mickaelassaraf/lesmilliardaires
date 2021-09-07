d5b1045394874454239d4f7e6aff476227e41690
mickaelassaraf <58734606+mickaelassaraf@users.noreply.github.com>
2021-09-07 11:47:35 +0200

    Create accès.py

---
+++ accès.py
@@ -0,0 +1,19 @@
+import requests
+
+params = {
+  'access_key': '7d337b020de9482a952705474ed9f009'
+}
+
+api_result = requests.get('https://api.aviationstack.com/v1/flights', params)
+
+api_response = api_result.json()
+
+for flight in api_response['results']:
+    if (flight['live']['is_ground'] is False):
+        print(u'%s flight %s from %s (%s) to %s (%s) is in the air.' % (
+            flight['airline']['name'],
+            flight['flight']['iata'],
+            flight['departure']['airport'],
+            flight['departure']['iata'],
+            flight['arrival']['airport'],
+            flight['arrival']['iata']))
